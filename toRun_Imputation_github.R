06e92e533384e86a3a852377c2e38ad7697e6f64
AStubbusch <astrid.stubbusch@bucet.de>
2019-05-13 09:53:44 +0200

    Imputation
    To make age prediction possible despite missing methylation information for any of the 329 'clock sites' used by the clock of Stubbs et al. (2017), we implemented an imputation step (kNN imputation) prior to age prediction. All of the original 18,000 CpG sites on which the clock of Stubbs et al. (2017) was based will be imputed based on the training data employed by the Stubbs2017 clock as well as the users input .cov files. Therefore, giving several .cov files as input instead of a single .cov file might improve imputation results very slightly.

---
+++ toRun_Imputation_github.R
@@ -0,0 +1,27 @@
+##Install dependencies, commented incase dependencies are already present.
+# source("https://bioconductor.org/biocLite.R")
+# install.packages("knitr", repos='http://cran.us.r-project.org')
+# install.packages("rmarkdown", repos='http://cran.us.r-project.org')
+# biocLite("preprocessCore")
+# install.packages("installr")
+# require(installr)
+# install.pandoc()
+
+#Put here the location of the package
+setwd("./PredictionPackage_20170215/")
+
+RdataF = "./PredictionPackage_20170215.Rdata"
+functionFile = "./PredictorFunctions.R"
+ImputationTrainingMatrix <- "./TrainingData_Babraham_Reizel_Cannon.txt"
+covFol = "./bismarkFiles/"
+readDepth = 5
+
+params = list(covFolder = covFol, RdataFile =RdataF, sourceFunctions=functionFile, ReadDepth = readDepth, ImputationTrainingMatrix = ImputationTrainingMatrix)
+
+#Render mark down
+rmarkdown::render('./PredictingAgeLeftOutSample.Rmd', 
+                  params = params,
+                  output_file = paste('./PredictingAgeLeftOutSample.html',sep=""))
+rmarkdown::render('./ImputationAndPrediction_github.Rmd',
+                  params = params,
+                  output_file = paste('./ImputationAndPrediction.html',sep=""))
